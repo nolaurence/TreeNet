b61f8f8cd24865f18ef8bd3e972d36a897893145
nolaurence <nolaurence@outlook.com>
2020-04-17 12:14:52 +0800

    add loss visualization

--- code/train.py
+++ code/train.py
@@ -2,7 +2,8 @@ import torch
 import time
 from torch import nn, optim
 from torch.utils import data as Data
-from network import Net_R, Net_A
+import matplotlib.pyplot as plt
+from network import Net
 from preprocess import trainset, testset
 # from eval import evaluation
 
@@ -12,8 +13,8 @@ BATCH_SIZE = 32
 SAMPLE_LENGTH = 8  # this parameters shouldn't be modified.
 NUM_RESIDENT = 2
 NUM_ACTIVITY = 15
-LEARNING_RATE = 0.01
-L2_WEIGHT = 1e-3
+LEARNING_RATE = 1e-4
+L2_WEIGHT = 1e-4
 N_EPOCHS = 10
 
 
@@ -28,7 +29,7 @@ def train():
     # model = Net(NUM_RESIDENT, NUM_ACTIVITY)
     # if torch.cuda.is_available():
 
-    modelR = Net_R(NUM_RESIDENT)
+    modelR = Net(NUM_RESIDENT, BATCH_SIZE)
     modelR.to(device)
 
 
@@ -38,48 +39,50 @@ def train():
 
     # training stage for resident
     for epoch in range(N_EPOCHS):
-        running_loss = 0
-        # for i in range(len(train_x)):
-        #     running_loss = 0
-        #     x = torch.from_numpy(train_x[i])
-        #     residents = torch.from_numpy(train_y[i][:, 0])
-        #     print(x.shape, residents.shape)
-        #     dataset = Data.TensorDataset(x, residents)
-        #     train_loader = Data.DataLoader(dataset=dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=1)
-
+        running_loss = []
         for i, data in enumerate(train_loader):
+            optimizer.zero_grad()
             inputs, labels = data
-            # inputs.to(device)
-            # labels.to(device)
+            if inputs.shape[0] < BATCH_SIZE:
+                continue
             outputs = modelR(inputs.cuda())
+            # print(outputs)
+            # print(labels[:, 0])
             loss = loss_fn(outputs, labels[:, 0].cuda())
             loss.backward()
             optimizer.step()
-            running_loss += loss.item()
+            running_loss.append(loss.item())
 
-            print('epoch {} batch{} resident loss: {:.4f}'.format(epoch + 1, i,running_loss / BATCH_SIZE))
+            print('\r', 'epoch {} batch {} resident loss: {:.4f}'.format(epoch + 1, i, loss.item()), end='')
 
-    torch.save(modelR, '../weights/parameters' + time.strftime('modelR-%Y-%m-%d_%H:%M:%S',
+    # plot code
+    x = range(len(running_loss))
+    plt.plot(x, running_loss)
+    plt.show()
+    torch.save(modelR, '../weights/parameters' + time.strftime('modelR-%Y-%m-%d_%H-%M-%S',
                                                                time.localtime(time.time())) + '.pkl')
 
-    modelA = Net_A(NUM_ACTIVITY)
+    modelA = Net(NUM_ACTIVITY, BATCH_SIZE)
     modelA.to(device)
     optimizer_A = optim.Adam(modelR.parameters(), lr=LEARNING_RATE, weight_decay=L2_WEIGHT)
     lossA = nn.CrossEntropyLoss()
     # training stage for activity
     for epoch in range(N_EPOCHS):
-        running_loss = 0
+        running_loss1 = []
         for i, data in enumerate(train_loader):
+            optimizer.zero_grad()
             inputs, labels = data
+            if inputs.shape[0] < BATCH_SIZE:
+                continue
             outputs = modelA(inputs)
             loss = lossA(outputs, labels[:, 1])
             loss.backward()
             optimizer_A.step()
-            running_loss += loss.item()
+            running_loss1.append(loss.item())
 
-            print('epoch {}, batch {} activity loss: {:.4f}'.format(epoch + 1, i , running_loss / BATCH_SIZE))
+            print('epoch {}, batch {} activity loss: {:.4f}'.format(epoch + 1, i , loss.item()))
 
-    torch.save(modelR, '../weights/parameters' + time.strftime('modelA-%Y-%m-%d_%H:%M:%S',
+    torch.save(modelR, '../weights/parameters' + time.strftime('modelA-%Y-%m-%d_%H-%M-%S',
                                                                time.localtime(time.time())) + '.pkl')
     print('Done.')
 
